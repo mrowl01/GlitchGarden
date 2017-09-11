494307405574d207650a88aa1bce5461babf1a36
mrowl01 <christopherortega56@gmail.com>
2017-09-10 21:48:02 -0500

    IMportToEmpty

---
+++ Glitch Garden/Assets/Scripts/MusicBoss.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicBoss : MonoBehaviour 
+{
+
+	public AudioClip [] levelMusic ; 
+	AudioSource audioSource ;
+
+	void Awake ()
+	{
+		DontDestroyOnLoad (gameObject); 
+		// Keeps this audio source object forever
+	}
+
+	void Start ()
+	{
+		audioSource = gameObject.GetComponent<AudioSource> ();
+		SceneManager.sceneLoaded += OnLevelLoaded;
+	}
+	void OnLevelLoaded (Scene scene, LoadSceneMode mode)// mode is just a delegate required in order for SceneManger.SceneLoaded to give us the current scene we are on (it is a delegate) 
+	{
+		AudioClip audioClip = levelMusic [scene.buildIndex]; // using scene given to us by scenemanager and delegate we make the music the array + the level  and play clip 
+		if (audioClip) 
+		{
+			audioSource.clip = audioClip; 
+			audioSource.loop = true;
+			audioSource.Play (); 
+		}
+			
+	}
+
+}
---
+++ Glitch Garden/Assets/Scripts/SceneBoss.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneBoss : MonoBehaviour 
+{
+	public float splashLoadTime = 3f; 
+
+	void Start ()
+	{
+		LoadInTime (splashLoadTime); 
+	}
+
+	public void SwitchToScene ( string theScene)
+	{
+		SceneManager.LoadScene (theScene); 
+	}
+	public void LoadNextScene()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+
+	}
+	public void QuitGame ()
+	{
+		Application.Quit (); 
+	}
+	public void LoadInTime (float theTime)
+	{
+		Invoke ("LoadNextScene", theTime);
+	}
+	public int SceneImOnNow ()
+	{
+		int myCurrentScene = SceneManager.GetActiveScene().buildIndex; 
+		return myCurrentScene;
+	}
+
+}
